d0269385ab9eb01469a25a99aaaf8abe5f5258e4
jnr_001 <obinyanemmanueljunior@gmail.com>
2024-08-12 17:49:52 +0100

    update-34

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
  function showContent(boxId){
+// Hide all content
+var contents = document.querySelectorAll('.to-do-container-box-2, .to-do-container-box-3, .to-do-container-box-4');
+contents.forEach(function(content) {
+    content.classList.remove('active');
+});
+
+
  // Show the selected content
  var selectedContent = document.getElementById(boxId);
  selectedContent.classList.add('active');
